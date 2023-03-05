5b6a25bbece1c4d84814ffb0a28318a43ad6c8cf
Hjanice <hjy9399@naver.com>
2023-03-05 21:11:34 +0900

    Initial commit

---
+++ src/test/java/dev/projectboard/MyProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
